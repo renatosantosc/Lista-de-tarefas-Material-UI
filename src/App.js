8c2449b89defb007f397af18a6b63456530e72e4
Renato Santos <renatosantosdeoliveirac@gmail.com>
2023-09-09 19:27:08 -0300

    Removendo arquivos padrões do react e inserindo o component task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/500.css';
+import { Container, Grid } from '@mui/material'
+import Task from './Components/Task';
+// import TaskList from './Components/TaskList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container maxWidth='lg' sx={{backgroundColor: 'transparent', justifyContent: 'center', height: '100vh'}}>
+        <Grid container spacing={8}>        
+          <Task />
+          {/* <TaskList /> */}
+        </Grid>
+      </Container>
+    </>
   );
 }
 
