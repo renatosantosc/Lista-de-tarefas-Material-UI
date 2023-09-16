ebeada9bdd3adaf666c83f35aabd9f1190a3a041
Renato Santos <renatosantosdeoliveirac@gmail.com>
2023-09-16 02:53:00 -0300

    Componente para deletar a tarefa selecionada

---
+++ src/Components/Delete.jsx
@@ -0,0 +1,36 @@
+import { Typography, Dialog, DialogTitle, DialogContent, DialogActions, Button, Grid, Divider, Paper } from '@mui/material'
+
+
+export default function Delete(props){
+
+    const Delete = key =>{
+       const del = props.list.filter ( itemObj =>{
+            return itemObj.key !== key
+        })
+        props.setList(del)
+        props.setDel(false)
+    }
+
+    return(
+        <>
+            <Grid container justifyContent={'center'}>
+                <Grid item xs={12}>
+                    <Dialog open={props.del} onClose={()=>{props.setDel(false)}} fullWidth>
+                        <Paper sx={{backgroundColor: props.userData.theme, width: '100%'}}>
+                            <DialogTitle textAlign={ 'center' }>Deseja excluir ?</DialogTitle>
+                            <Divider />
+                            <DialogContent>
+                                <Typography textAlign={'center'} fontWeight={'bold'}>{props.userData.title}</Typography>
+                                <Typography textAlign={'center'}>{props.userData.name}</Typography>
+                            </DialogContent>
+                            <DialogActions sx={{marginTop: 2}}>
+                                <Button variant='contained' onClick={()=>{Delete(props.userData.id)}} color='error'>Excluir</Button>
+                                <Button onClick={()=>{props.setDel(false)}} sx={{color: 'black'}}>Cancelar</Button>
+                            </DialogActions>
+                        </Paper>
+                    </Dialog>
+                </Grid>
+            </Grid>
+        </>
+    )
+}
