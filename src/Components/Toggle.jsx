cf37baebfc4c9fb2ad8a1afb9b0f2cf295716fdf
Renato Santos <renatosantosdeoliveirac@gmail.com>
2023-09-22 14:09:06 -0300

    Modificando a lista, retirando um botão e inserindo um link, no link foi colocado o link do github e no botão a função de troca de tema, em ambos foi colocado um condição para definir a cor de acordo com o tema, mesma condição inserida no background da lista

--- src/Components/Toggle.jsx
+++ src/Components/Toggle.jsx
@@ -1,7 +1,8 @@
-import { Box, Button, Grid, List, ListItem, ListItemIcon, ListItemText, ListItemButton, Drawer } from '@mui/material'
+import { Box, Button, Grid, List, ListItem, ListItemIcon, ListItemText, ListItemButton, Drawer, Link, IconButton } from '@mui/material'
 import GitHubIcon from '@mui/icons-material/GitHub';
-// import DarkModeIcon from '@mui/icons-material/DarkMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
+import MenuIcon from '@mui/icons-material/Menu';
 import { useState, Fragment } from 'react'
 
 export default function Toggle(props){
@@ -21,23 +22,35 @@ export default function Toggle(props){
         onKeyDown={toggleDrawer(anchor, false)}
         >
             <List sx={{backgroundColor: props.mode ? '#e2e2e2' : '#262d35', height: '100vh'}}>
-                {['Github', 'Dark/Light'].map((text, index) =>(
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon> {index % 2 === 0 ? <GitHubIcon /> : <LightModeIcon />} </ListItemIcon>
-                            <ListItemText primary={text} />
+                    <ListItem disablePadding>
+                        <ListItemButton sx={{color: props.mode ? '#262d35' : '#e3eae9'}} onClick={()=>{ props.setMode(!props.mode) }}>
+                            <ListItemIcon> {props.mode 
+                                ? <DarkModeIcon sx={{color: props.mode ? '#262d35' : '#e3eae9'}}/> 
+                                : <LightModeIcon sx={{color: props.mode ? '#262d35' : '#e3eae9'}}/> } 
+                            </ListItemIcon>
+                            <ListItemText primary={'Dark/Light'} />
                         </ListItemButton>
                     </ListItem>
-                ))}
+
+                    <ListItem disablePadding>
+                        <Link href='https://github.com/renatosantosc' underline='none' target='_blank' rel='noopener' sx={{width: '100%'}} >
+                            <ListItemButton sx={{color: props.mode ? '#262d35' : '#e3eae9'}}>
+                                <ListItemIcon> <GitHubIcon sx={{color: props.mode ? '#262d35' : '#e3eae9'}} /> </ListItemIcon>
+                                <ListItemText primary={'GitHub'} />
+                            </ListItemButton>
+                        </Link>
+                    </ListItem>
             </List>
         </Box>
     )
     return(
         <Grid item lg={4} md={4} sm={4} xs={4} textAlign={'end'} >
             {['right'].map((anchor) => (
-                <Fragment>
-                    <Button onClick={toggleDrawer(anchor, true)}>X</Button>
-                    <Drawer
+                <Fragment key={anchor}>
+                    <Button onClick={toggleDrawer(anchor, true)}> 
+                        <IconButton sx={{color: props.mode ? '#262d35' : '#e3eae9'}}> <MenuIcon /> </IconButton> 
+                    </Button>
+                    <Drawer 
                     anchor={anchor}
                     open={toggle[anchor]}
                     onClose={toggleDrawer(anchor, false)}
