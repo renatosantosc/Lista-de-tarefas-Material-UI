bb9cc86d568fb35abd34c566f08f62300d11daef
Renato Santos <renatosantosdeoliveirac@gmail.com>
2023-10-02 20:04:24 -0300

    Modificando o valor da condição do width e também o valor do MarginBottom do Box

--- src/Components/Save.jsx
+++ src/Components/Save.jsx
@@ -7,7 +7,7 @@ export default function Save(props){
         <>
            <Grid item xs={12} >
                 <Grid container justifyContent={'center'} >
-                    <Box sx={{width: props.width >= 535 ? '50%' : '100%', marginTop: '7px', marginBottom: '10px'}}>
+                    <Box sx={{width: props.width >= 560 ? '50%' : '100%', marginTop: '7px', marginBottom:'20px'}}>
                     <Collapse in={props.success} >
                         <Alert onClose={props.success} severity='success' sx={{width: '92%', alignItems: 'center', fontSize: '1.1rem'}} action={
                         <IconButton onClick={()=>{ props.setSuccess(false) }}> <CloseIcon sx={{marginBottom: '6px'}}/> </IconButton> }>
